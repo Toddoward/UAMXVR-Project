160479a0ff1dfba4a7845b9850c3435df5407f5a
MoccaHazel <101385960+MoccaHazel@users.noreply.github.com>
2024-04-14 02:47:06 +0900

    Update 5.2.1 and add SWARM

---
+++ Source/UAMXVRProject/UAMXVRProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UAMXVRProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UAMXVRProject, "UAMXVRProject" );
