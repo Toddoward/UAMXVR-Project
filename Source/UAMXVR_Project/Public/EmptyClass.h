ad7c6ad8e4498beed9dd2632d746159d723b9fdf
MoccaHazel <wltn1937@gmail.com>
2024-05-13 23:28:59 +0900

    Add AirSim

---
+++ Source/UAMXVR_Project/Public/EmptyClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class UAMXVR_PROJECT_API EmptyClass
+{
+public:
+	EmptyClass();
+	~EmptyClass();
+};
